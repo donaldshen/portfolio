863fe1bd2bd0b347840ea535c84f6d770e7b18c9
donald <825870831@qq.com>
2016-08-30 09:10:23 +0800

    feat: Add builder.js

---
+++ js/builder.js
@@ -0,0 +1,25 @@
+'use strict';
+/**
+* @file Dynamicly generate the project block
+* @author Donald Shen <donald930224@hotmail.com>
+*/
+
+/**
+* @global
+*/
+
+function buildProjectHTML(project) {
+    var projectHTML = '<article class="project"><figure><picture><source media="(min-width: 750px)" srcset="img/%img%-large.jpg" /><source media="(min-width: 500px)" srcset="img/%img%-medium.jpg" /><img src="img/%img%-small.jpg" alt="%alt%" /></picture><figcaption><a class="zocial-github" href="%url%" target="_blank"></a>%description%</figcaption></figure></article>';
+    projectHTML.replace('%img%', project.img);
+    projectHTML.replace('%alt%', project.alt);
+    projectHTML.replace('%url%', project.url);
+    projectHTML.replace('%description%', project.description);
+    return projectHTML;
+}
+
+(function () {
+    var content = $('content');
+    projects.forEach(function (p) {
+        content.append(buildProjectHTML(p));
+    });
+})();
