4c42831ba487fe5bcc20098fc53b66b5edcb550b
donald <825870831@qq.com>
2016-08-31 08:41:58 +0800

    refactor: Move build function from global scope

--- js/builder.js
+++ js/builder.js
@@ -5,16 +5,17 @@
 */
 
 
-function buildProjectHTML(project) {
-    var projectHTML = '<article class="project"><figure><picture><source media="(min-width: 750px)" srcset="img/%img%-large.jpg" /><source media="(min-width: 500px)" srcset="img/%img%-medium.jpg" /><img src="img/%img%-small.jpg" alt="%alt%" /></picture><figcaption><a class="zocial-github" href="%url%" target="_blank"></a>%description%</figcaption></figure></article>';
-    projectHTML.replace('%img%', project.img);
-    projectHTML.replace('%alt%', project.alt);
-    projectHTML.replace('%url%', project.url);
-    projectHTML.replace('%description%', project.description);
-    return projectHTML;
-}
 
 (function () {
+    function buildProjectHTML(project) {
+        var projectHTML = '<article class="project"><figure><picture><source media="(min-width: 750px)" srcset="img/%img%-large.jpg" /><source media="(min-width: 500px)" srcset="img/%img%-medium.jpg" /><img src="img/%img%-small.jpg" alt="%alt%" /></picture><figcaption><a class="zocial-github" href="%url%" target="_blank"></a>%description%</figcaption></figure></article>';
+        projectHTML.replace('%img%', project.img);
+        projectHTML.replace('%alt%', project.alt);
+        projectHTML.replace('%url%', project.url);
+        projectHTML.replace('%description%', project.description);
+        return projectHTML;
+    }
+
     var content = $('content');
     projects.forEach(function (p) {
         content.append(buildProjectHTML(p));
